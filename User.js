1f5a04fe76cb2692c124058d66ad5938b68e9b65
Marc Pichler <marcpi@edu.aau.at>
2019-05-13 03:04:24 +0200

    finishing touches.

--- User.js
+++ User.js
@@ -1,19 +1,20 @@
 const uuidv4 = require('uuid/v4');
+const uuidv1 = require('uuid/v1');
 
 class User {
-    constructor(username, token = null) {
-        this.username = username;
+    constructor(username, token, date) {
+        this.username = username.replace(/</g, "&lt;").replace(/>/g, "&gt;");
         this.token = token;
         this.socket = null;
+        this.date = date;
     }
 
     generateToken() {
         this.token = uuidv4();
     }
 
-    generateUserInformation()
-    {
-        return new User(this.username);
+    generateUserInformation() {
+        return new User(this.username, null, this.date);
     }
 }
 
--- app.js
+++ app.js
@@ -15,40 +15,40 @@ app.get('/', function (req, res) {
 });
 
 function getUsername(token) {
-    for(let socketid in global.users)
-    {
+    for (let socketid in global.users) {
         //console.log(global.users[socketid]);
-        if(global.users[socketid].token === token)
+        if (global.users[socketid].token === token)
             return global.users[socketid].username;
     }
     return null;
 }
 
 function disconnectUser(socket) {
-
+    let user = global.users[socket];
+    distributeMessage({
+        username: user.username,
+        content: user.username + " has left.",
+        date: (new Date()).toTimeString()
+    })
     delete global.users[socket];
-    //TODO: synchronize.
     reportOnlineUsers();
 
 }
 
 function reportOnlineUsers() {
     let userList = [];
-   // console.log(global.users);
-    for(let key in global.users)
-    {
+    // console.log(global.users);
+    for (let key in global.users) {
         userList.push(global.users[key].generateUserInformation())
     }
-    for(let key in global.users)
-    {
+    for (let key in global.users) {
         global.users[key].socket.emit("online_users", userList);
     }
 }
 
 function distributeMessage(message) {
     global.messageHistory.push(message);
-    for(let key in global.users)
-    {
+    for (let key in global.users) {
         global.users[key].socket.emit("new_message", message)
     }
 }
@@ -56,13 +56,19 @@ function distributeMessage(message) {
 io.on('connection', function (socket) {
     socket.emit('request_registration', {});
     socket.on('register', function (user) {
-        let serverUser = new User(user.username);
+        let serverUser = new User(user.username, null, (new Date()).toTimeString());
         serverUser.generateToken();
+        let currentDate = new Date();
         socket.emit('registration_response', serverUser);
         socket.emit('message_history', global.messageHistory);
         serverUser.socket = socket;
         global.users[socket.id] = serverUser;
         reportOnlineUsers();
+        distributeMessage({
+            username: serverUser.username,
+            content: serverUser.username + " joined the chat.",
+            date: (new Date()).toTimeString()
+        })
     });
 
     socket.on('disconnect', function () {
@@ -71,8 +77,12 @@ io.on('connection', function (socket) {
 
     socket.on('message', function (clientMessage) {
         let username = getUsername(clientMessage.token);
-        if(username != null)
-            distributeMessage({username: username, content: clientMessage.content});
+        if (username != null)
+            distributeMessage({
+                username: username,
+                content: clientMessage.content.replace(/</g, "&lt;").replace(/>/g, "&gt;"),
+                date: (new Date()).toTimeString()
+            });
         else
             console.error("user not registered.");
     });
